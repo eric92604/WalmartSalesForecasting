c2873be03b552ad5de9a04082389ff97831e3b26
eric92604 <eric92604@gmail.com>
2024-05-15 13:38:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ if __name__ == "__main__":
     try:
 
         main_functions.main(source_dir,
-                            timeseriesdataset_from_file=True,
-                            find_optimal_lr=False,
-                            find_optimal_hyperparameters=True,
-                            batch_size=128,
-                            worker_size=4,
-                            persistent_workers=True,
-                            num_epochs=5
+                            test=True,  # Enable using much smaller testing dataset
+                            timeseriesdataset_from_file=False,
+                            # Load timeseriesdataset from file rather than generating it, cuts down initial load time
+                            find_optimal_lr=False,  # Enable finding optimal learning rate
+                            find_optimal_hyperparameters=False, # Enable finding optimal hyperparameters
+                            find_optimal_batch_size=False,  # currently debugging variable batch size hparam
+                            batch_size=1024,  # Batch size for data loaders
+                            worker_size=4,  # Number of workers for data loaders
+                            persistent_workers=True,  # Should leave as true unless disabling multiprocessing
+                            num_epochs=10  # Number of epochs to fit model
                             )
     except Exception as err:
         logging.info({err})
