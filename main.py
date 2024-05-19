d9cc6980637eb1b61009cc985449634677ab6f13
eric92604 <eric92604@gmail.com>
2024-05-19 01:39:44 -0700

    Enabled continuing training from latest checkpoint

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 # Task: To forecast hierarchical sales for multiple Walmart stores in California, Texas, and Wisconsin.
 # The dataset comes from Kaggle: M5 Forecasting - Accuracy Competition.
+# Enable tensorboard dashboard through "python -m tensorboard.main --logdir=lightning_logs/"
 
 if __name__ == "__main__":
     source_path = Path(__file__).resolve()
@@ -14,16 +15,17 @@ if __name__ == "__main__":
     try:
 
         main_functions.main(source_dir,
-                            test=True,  # Enable using much smaller testing dataset
-                            timeseriesdataset_from_file=False,
+                            test=False,  # Enable using much smaller testing dataset
+                            timeseriesdataset_from_file=True,
                             # Load timeseriesdataset from file rather than generating it, cuts down initial load time
                             find_optimal_lr=False,  # Enable finding optimal learning rate
-                            find_optimal_hyperparameters=False, # Enable finding optimal hyperparameters
+                            find_optimal_hyperparameters=False,  # Enable finding optimal hyperparameters
                             find_optimal_batch_size=False,  # currently debugging variable batch size hparam
+                            from_checkpoint=True,
                             batch_size=1024,  # Batch size for data loaders
-                            worker_size=4,  # Number of workers for data loaders
+                            worker_size=8,  # Number of workers for data loaders
                             persistent_workers=True,  # Should leave as true unless disabling multiprocessing
-                            num_epochs=10  # Number of epochs to fit model
+                            num_epochs=50  # Number of epochs to fit model
                             )
     except Exception as err:
         logging.info({err})
